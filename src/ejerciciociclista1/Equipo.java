8f048d5393d49deb6a0753a66beacd0b15dd0899
MariadelMarPerez <mariadelmarperezmontengro@gmail.com>
2025-04-07 21:23:06 -0500

    Elimino de la clase el metodo main

--- src/ejerciciociclista1/Equipo.java
+++ src/ejerciciociclista1/Equipo.java
@@ -83,27 +83,6 @@ public class Equipo {
         }
         Equipo.tiempoTotalEquipo = total;
     }
-    
-    public static void main(String[] args) {
-        Equipo equipo = new Equipo("Equipo Aguilas", "Perú");
-
-        Velocista velocista = new Velocista(540.5, 1, "Lucas");
-        velocista.setVelocidadPromedioSprint(56.0);
-        velocista.setTiempoAcumulado(120);
-
-        Escalador escalador = new Escalador(4.6f, 9.0f, 4, "Daniel");
-        escalador.setTiempoAcumulado(130);
-
-        Contrarrelojista contrarrelojista = new Contrarrelojista(80.0, 2, "Juan");
-        contrarrelojista.setTiempoAcumulado(115);
-
-        equipo.añadirCiclista(velocista);
-        equipo.añadirCiclista(escalador);
-        equipo.añadirCiclista(contrarrelojista);
-        equipo.calcularTiempoTotal();
-        equipo.listarNombresCiclistas(); 
-
-        System.out.println("Tiempo total del equipo: " + equipo.getTiempoTotalEquipo());
-    }
+   
     
 }
